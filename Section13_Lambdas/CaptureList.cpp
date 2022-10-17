a0752fcbeffcc3be85d2c68096060fab5f665edc
Denis Kruchok <denis.kruchok@globant.com>
2022-10-17 22:08:19 +0300

    Capture List part 3

--- Section13_Lambdas/CaptureList.cpp
+++ Section13_Lambdas/CaptureList.cpp
@@ -38,9 +38,58 @@ void PrintArray(T(&arr)[size])
     std::cout << std::endl;
 }
 
+class Product
+{
+    std::string Name;
+    float Price;
+
+public:
+    Product(const std::string& InitName, float InitPrice)
+        : Name(InitName)
+        , Price(InitPrice)
+    {}
+
+    void AssignFinalPrice()
+    {
+        float Taxes[]{ 12, 5, 5 }; // in %
+        float BasePrice{ Price };
+
+        ForEach(Taxes, [BasePrice, this](float Tax)
+            {
+                float TaxedPrice = BasePrice * Tax / 100;
+                Price += TaxedPrice;
+            });
+    }
+
+    float GetPrice()
+    {
+        return Price;
+    }
+};
+
 
 void CaptureListMain()
-{
+{    
+    atexit([]()
+        {
+            std::cout << "Program is exitting..." << std::endl;
+        }
+    );
+
+    Product p1{ "Phone", 500 };
+    p1.AssignFinalPrice();
+    std::cout << p1.GetPrice() << std::endl;
+
+    [](int x)
+    {
+        x *= 2;
+
+        [](int x)
+        {
+            std::cout << x << std::endl;
+        }(x);
+    }(5);
+
     int arr[]{ 1,6,8,4,9,555,3,0,5,6 };
 
     PrintArray(arr);
@@ -53,11 +102,18 @@ void CaptureListMain()
     Sort(arr, comp);
     PrintArray(arr);
 
-    int offset = 3;
-    ForEach(arr, [offset](auto& x) mutable
+    int offset = 0;
+    //ForEach(arr, [offset](auto& x) mutable
+    //    {
+    //        x += offset;
+    //        ++offset;
+    //    });
+    //PrintArray(arr);
+
+    int sum{};
+    ForEach(arr, [=, &sum](auto& x)
         {
-            x += offset;
-            ++offset;
+            sum += x;
         });
-    PrintArray(arr);
+    std::cout << sum << std::endl;
 }
