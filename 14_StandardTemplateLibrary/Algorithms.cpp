9cbb477c1b4e0fd43caeafcfcc997f432c3436b2
Denis Kruchok <denis.kruchok@globant.com>
2022-10-18 15:13:39 +0300

    Added algorithm examples (part 2).

--- 14_StandardTemplateLibrary/Algorithms.cpp
+++ 14_StandardTemplateLibrary/Algorithms.cpp
@@ -31,11 +31,24 @@ public:
 	}*/
 };
 
+struct EmpIds
+{
+	std::vector<int> Ids;
+	void operator()(const Employee& emp)
+	{
+		if (emp.GetProgramLanguage() == "C++")
+		{
+			Ids.push_back(emp.GetId());
+		}
+	}
+};
+
 void Vector()
 {
 	std::vector<Employee> Employees{
 		Employee{"Rachel", 1314, "Python"},
 		Employee{"Andrew", 101, "C++"},
+		Employee{"Denis", 9, "C++"},
 		Employee{"Bob", 11, "Java"} };
 
 	std::sort(Employees.begin(), Employees.end(),
@@ -51,6 +64,51 @@ void Vector()
 			<< " | Name : " << emp.GetName()
 			<< " | Program Language : " << emp.GetProgramLanguage()
 			<< std::endl;
+	}
+
+	int CppCount{};
+	for (const auto& emp : Employees)
+	{
+		if (emp.GetProgramLanguage() == "C++")
+		{
+			++CppCount;
+		}
+	}
+	std::cout << "By hand loop " << std::endl;
+	std::cout << "Count : " << CppCount << std::endl;
+
+	CppCount = std::count_if(Employees.begin(), Employees.end(),
+		[](const auto& emp)
+		{
+			return emp.GetProgramLanguage() == "C++";
+		});
+
+	std::cout << "By algorithm count " << std::endl;
+	std::cout << "Count : " << CppCount << std::endl;
+
+	std::string DeisredProgramLanguage = "Java";
+	auto itr = std::find_if(Employees.begin(), Employees.end(),
+		[&DeisredProgramLanguage](const auto& emp)
+		{
+			return emp.GetProgramLanguage() == DeisredProgramLanguage;
+		});
+
+	if (itr != Employees.end())
+	{
+		std::cout << "Found : " << itr->GetName() << " is a Java programmer" << std::endl;
+	}
+
+	std::for_each(Employees.begin(), Employees.end(),
+		[](const auto& emp)
+		{
+			std::cout << emp.GetName() << std::endl;
+		});
+
+	auto FoundIds = std::for_each(Employees.begin(), Employees.end(), EmpIds());
+
+	for (const auto& id : FoundIds.Ids)
+	{
+		std::cout << "Id : " << id << std::endl;
 	}
 }
 
@@ -82,5 +140,5 @@ void Set()
 
 void AlgorithmsMain()
 {
-	Set();
+	Vector();
 }
